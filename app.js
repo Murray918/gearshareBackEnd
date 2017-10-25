3e2be04328f37ef23383b84fc7a5b8bf14ff32bb
Andrew Murray <darbyguitars@gmail.com>
2017-10-25 14:25:05 -0500

    creating a sql script to handle some initial users and tables

---
+++ app.js
@@ -0,0 +1,118 @@
+const models = require("./models"),
+  Sequelize = require("sequelize"),
+  express = require("express"),
+  bodyParser = require("body-parser");
+let app = express();
+
+//boiler plate body parser can talk to the html and get it recieve it in app.js
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+console.log("party party");
+
+//this creates the table for all the products
+function createProduct() {
+  const product = models.Product.build({
+    make: "Sure",
+    modelNum: "SM7B",
+    info: "this is a micrphpne spoken word"
+    // tags: ['mic', 'micrphpne', 'live']
+  });
+  product.save().then(newProduct => {
+    console.log(newProduct.dataValues);
+  });
+}
+// createProduct();
+
+function listProducts() {
+  models.Product.findAll().then(function(products) {
+    products.forEach(function(product) {
+      console.log(product.dataValues);
+    });
+  });
+}
+
+listProducts();
+
+function deleteProduct () {
+  models.Product.findById(3)
+    .then( (product) => {
+      console.log(product);
+      product.destroy()
+    })
+}
+
+deleteProduct();
+
+function createUser() {
+  const user = models.User.build({
+    name: "Sarah Shuey",
+    userName: "fluffyPuppy",
+    bio: "I like dogs you fool"
+    // tags: ['mic', 'micrphpne', 'live']
+  });
+
+  user.save().then(function(newUser) {
+    console.log(newUser.dataValues);
+  });
+}
+
+// createUser();
+
+function listUsers() {
+  models.User.findAll().then(function(users) {
+    users.forEach(function(user) {
+      console.log(user.dataValues);
+    });
+  });
+}
+
+listUsers();
+
+function updateUser() {
+  models.User
+    .update(
+      {
+        name: "Bailey Marie"
+      },
+      {
+        where: {
+          id: 1
+        }
+      }
+    )
+    .then(result => {
+      models.User.findById(1).then(user => {
+        // console.log(user);
+      });
+    });
+}
+
+updateUser();
+
+function deleteUser (){
+  models.User.findById(3)
+    .then( (user) => {
+      console.log(user);
+      user.destroy()
+    })
+}
+
+// deleteUser();
+
+app.listen(3000, () => {
+  console.log("successfully started Express Application");
+});
+
+process.on("SIGINT", () => {
+  console.log("\nshutting down");
+  const index = require("./models/index");
+  index.sequelize.close();
+
+  // give it a second
+  setTimeout(() => {
+    console.log("process exit");
+    process.exit(0);
+  }, 1000);
+});
---
+++ capstone.sql
@@ -0,0 +1,36 @@
+CREATE TABLE users (
+  id SERIAL,
+  first_name VARCHAR(35) NOT NULL,
+  last_name VARCHAR(35) NOT NULL,
+  username VARCHAR(20) NOT NULL UNIQUE,
+  password VARCHAR(20) NOT NULL UNIQUE,
+  created_at timestamp NOT NULL,
+  PRIMARY KEY (id)
+
+);
+
+INSERT INTO users (first_name, last_name, username, password, created_at) VALUES ('Bailey', 'Bryant', 'fluffykitty', '1234567', current_timestamp);
+INSERT INTO users (first_name, last_name, username, password, created_at) VALUES ('Sam', 'Kapila', 'uiqueen', '23456789', current_timestamp);
+INSERT INTO users (first_name, last_name, username, password, created_at) VALUES ('Sarah', 'Shuey', 'alpha', '34567890', current_timestamp);
+INSERT INTO users (first_name, last_name, username, password, created_at) VALUES ('Colton', 'Dowling', 'colot' '56787890', current_timestamp);
+INSERT INTO users (first_name, last_name, username, password, created_at) VALUES ('Anthony', 'Silva' 'ajsilva', '12345678', current_timestamp, current_timestamp);
+
+CREATE TABLE microphone (
+  user_id int NOT NULL,
+  id SERIAL PRIMARY KEY,
+  make VARCHAR(40) NOT NULL,
+  modle VARCHAR(40) NOT NULL,
+  serial_number VARCHAR(100) NULL UNIQUE,
+  description VARCHAR(300) NULL,
+  power_source VARCHAR(40),
+  created_at timestamp NOT NULL,
+  PRIMARY KEY (user_id),
+  CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+
+INSERT INTO todos (make, modle, serial_number, description, power_source, created_at,) VALUES ('walk dog', 2, current_timestamp);
+INSERT INTO todos (title, priority, created_at) VALUES ('clean house', 3, current_timestamp);
+INSERT INTO todos (title, priority, created_at) VALUES ('check mail', 4, current_timestamp);
+INSERT INTO todos (title, priority, created_at) VALUES ('pay bills', 5, current_timestamp);
+INSERT INTO todos (title, priority, created_at, completed_at) VALUES ('call mom', 3, current_timestamp, current_timestamp);
